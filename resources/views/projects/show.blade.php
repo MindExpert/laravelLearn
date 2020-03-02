24d0ab16f528051f7dd8bd5f4bc13b0fdbc41756
xhoanaqesja <xhoana2009@live.com>
2020-03-02 17:16:49 +0100

    Fixed tasks

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,15 +3,18 @@
 @section('content')
     <h1 class="title">{{ $project->title}}</h1>
     <div class="content">{{ $project->description}}</div>
-    @if ($project->tasksb )
-        <div>
-            @foreach ($project->tasks as $task)
-                <li> {{ $task->description}}</li>        
-            @endforeach
-        </div>
-    @endif
-    
     <p>
         <a href="/projects/{{ $project->id }}/edit">Edit</a>
     </p>
+
+    @if ($project->tasks->count() )
+        <div class="content">
+            <ul >
+                @foreach ($project->tasks as $task)
+                    <li> {{ $task->description}}</li>        
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    
 @endsection
