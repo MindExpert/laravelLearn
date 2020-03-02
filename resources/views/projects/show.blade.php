396fb31956384bd1e4d8f735cc45d7be063f0544
Elvis <elvisi.muchasaj@gmail.com>
2020-03-02 16:04:55 +0100

    Added tasks and Eloquent relationShips

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,6 +3,14 @@
 @section('content')
     <h1 class="title">{{ $project->title}}</h1>
     <div class="content">{{ $project->description}}</div>
+    @if ($project->tasksb )
+        <div>
+            @foreach ($project->tasks as $task)
+                <li> {{ $task->description}}</li>        
+            @endforeach
+        </div>
+    @endif
+    
     <p>
         <a href="/projects/{{ $project->id }}/edit">Edit</a>
     </p>
